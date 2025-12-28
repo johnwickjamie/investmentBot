87b10d2639526a393793967c45ce092bf2f181b7
johnwickjamie <99115236+johnwickjamie@users.noreply.github.com>
2025-12-28 17:55:47 +0000

    Add files via upload

---
+++ investmentBot/src/main/java/telegramAPI/globalConfig.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package telegramAPI;
+
+/**
+ *
+ * @author james
+ */
+public class globalConfig {
+    public static String[] APITracker = new String[2]; //used to store the number of requests made in a min and the time of the last request. [0] = num of requests, [1] =  time of last request
+
+    
+}
